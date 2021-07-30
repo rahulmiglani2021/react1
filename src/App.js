8d3b34c3ed106b3bf3968c34818b3db1cfc2da9a
Rahul Miglani <rahul.miglani@craverapp.com>
2021-07-29 17:36:14 -0700

    Hello World
    First Changes to React Project

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>React Project 1</code> 
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lets do this !
         </a>
       </header>
     </div>
